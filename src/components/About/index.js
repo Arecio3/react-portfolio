1d8fa938df2814a84c106087620d2438fe86690e
Arecio Canton <cuba289@gmail.com>
2021-05-25 22:54:06 -0400

    fixed about me css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,17 +6,19 @@ import './style.css'
 
 const About = () => {
     return (
-            <article>
-                <h1 className='aboutMeHeader'>About Me;</h1>
-               <div className='bio'>
+        <article className='bio'>
+            <h1 className='aboutMeHeader'>About Me;</h1>
+            <div id='bioContent'>
                 <img src={selfie}></img>
-               <h1 className='bioHead'>Bio</h1>
-                <p>My name is <span>Arecio Canton</span>,  <br /> I'm striving to become a <span>full-stack developer</span> and my ultimate goal is to be able to work with Artificial Intelligence </p>
-                <ProgressBar />
-               </div>
-            
-            </article>
-     
+                <div id="biography">
+                    <h1 className='bioHead'>Bio</h1>
+                    <p>My name is <span>Arecio Canton</span>,  <br /> I'm striving to become a <span>full-stack developer</span> and my ultimate goal is to be able to work with Artificial Intelligence </p>
+                    <ProgressBar />
+                </div>
+            </div>
+
+        </article>
+
     )
 }
 
